00a63ea3dd05a7aa1aa37ae861660be1e61bc975
Ricardo Vargas <rkrdo.vrgs@gmail.com>
2018-03-04 15:58:15 -0500

    added resources from au-base

--- app/src/main.ts
+++ app/src/main.ts
@@ -20,7 +20,7 @@ export function configure(aurelia: Aurelia) {
     aurelia.use
         .standardConfiguration()
         //globalize features
-        //.feature("resources")
+        .feature("au-base/app/resources")
         //load plugins
         .plugin("aurelia-dialog")
         //.plugin("shared/redux-base/config", (createStore: IReduxStoreCreator) => createStore(reducers, environment.debug))
