2a75763ed3b3bba17bfbe6ed48403e52d9982325
Ricardo Vargas <rkrdo.vrgs@gmail.com>
2018-03-05 03:01:33 -0500

    added fileservice to details

--- templates/details/details.ts
+++ templates/details/details.ts
@@ -1,12 +1,13 @@
 import { inject } from "aurelia-framework";
 import { DbService } from "dataservices/db-service";
 import { Router } from "aurelia-router";
+import { FileService } from "au-base/app/dataservices/file-service";
 
-@inject(DbService, Router)
+@inject(DbService, FileService, Router)
 export class __modelNameCapitalize__Details {
     __modelNameCamelCase__: I__modelNameCapitalize__;
 
-    constructor(private db: DbService, private router: Router) { }
+    constructor(private db: DbService, private fileService: FileService, private router: Router) { }
 
     async activate({ __modelNameCamelCase__Id }) {
         this.__modelNameCamelCase__ = await this.db.__modelNamePluralCamelCase__.findById(__modelNameCamelCase__Id) || <I__modelNameCapitalize__>{};
