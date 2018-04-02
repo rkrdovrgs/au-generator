f47df8f10791ce4e2063045c0415e975b049ce97
Ricardo Vargas <rkrdo.vrgs@gmail.com>
2018-04-01 22:14:00 -0400

    updated instructions

--- app/src/projects/project-details.ts
+++ app/src/projects/project-details.ts
@@ -17,6 +17,7 @@ export class ProjectDetails {
 
     setInstructions() {
         this.instructions = `
+        #Git & Github
         - Create a new repository at: https://github.com/new
 
         - Run the following commands:
@@ -30,6 +31,7 @@ export class ProjectDetails {
             5. git push --set-upstream origin master
             6. mkdir node_modules
 
+        #Dropbox
         - Make sure dropbox is running
         - Configure dropbox selective sync to avoid syncing the node_modules folder:
             On the system tray, locate the dropbox icon, 
@@ -39,11 +41,39 @@ export class ProjectDetails {
             Click "Selective Sync"
             Deselect "Apps/au-generator/${this.project.name}/node_modules"
 
+        #Running the app
         - Install the project dependencies by running:
             npm install
 
-        - To run the app:
+        - Run the app:
             npm run start:dev
+
+        #Heroku
+        - Create a new app at: https://dashboard.heroku.com/new-app
+            Strongly recommend to use the same name as the git repo name
+        - Go to newly created app
+        
+        - Go to the tab "Resources"
+        - On the "Add-ons" section, search for "mLab Mongo DB"
+        - Select "mLab Mongo DB", and when the pop-up opens, click "Provision"
+
+        - Go to the tab "Deploy
+        - On the deployment method select "Github"
+        - Search for the repo name: ${this.project.name}
+        - Click "Connect"
+
+        #Making changes to your app
+        - To start the app locally run
+            npm run start:dev
+        
+        - To upload a change to Github:
+            git add .
+            git commit -m "<message>"
+            git push
+
+        - To deploy the latest version of the app to heroku:
+            Go to Heroku at: https://dashboard.heroku.com/apps/${this.project.name}/deploy/github
+            Scroll down and click "Deploy Branch"
         `;
     }
 
